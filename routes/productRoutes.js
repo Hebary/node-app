01a566a25ae8006579aa3be3c9066c47e025bb5e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-01 12:39:45 +0800

    Delete routes directory

--- routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-import { getProducts } from '../controllers/productsController.js'
-const routerProducts = Router();
-
-routerProducts.get('/',getProducts)
-
-
-export default routerProducts;
-
